d61924969fb986f72b4676cf35e86d37126599fe
creaja <wnsgud5813@naver.com>
2022-08-28 15:45:55 +0900

    config file

---
+++ spring-config.sh
@@ -0,0 +1,7 @@
+echo "# spring-config" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/creaja/spring-config.git
+git push -u origin main
